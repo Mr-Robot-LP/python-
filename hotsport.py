25e127157f534e298105d48c8b8e9b4c0e4d4789
lapyaemaungmaung <lapyaemaungmaung8@gmail.com>
2022-01-04 02:09:31 +0630

    hello

---
+++ hotsport.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#2022.1.1
+#programmer zero
+#program hotsport enable or disable
+import os
+print("welcome my program")
+a='0'
+while a!=3:
+ print("hotsport start is 1")
+ print("hotsport stop is 2")
+ print("exit is 3")
+ a=input("please choice 1,2,3:  ")
+ if a=='1':
+   print("starting wifi hotsport please wait........ ......... ...........")
+    # HSname=input("Enter your hotsport name: ")
+   #  HSpassword=int(input("Enter your hotsport password: "))
+    # os.system("netsh wlan set hostednetwork mode=allow ssid=HSname key=HSpassword")
+   #  os.system("netsh wlan start hostednetwork")  	             
+ elif a==2:
+      #  print("stoping wifi hotsport please wait........ ......... ..........)   
+        # os.system("netsh wlan stop hostednetwork")
+     #elif a==3:
+         #  print("exiting program please wait ")
+           #	quit()   	
+    #else:
+      #print("bad input please try again(only 1,2,3) ") 
